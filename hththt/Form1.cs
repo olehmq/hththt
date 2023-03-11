edda7f25e6da5d4c9bbbaf9795f5b0c1cf2f708c
олег <@DESKTOP-SSTKF2A>
2023-03-11 17:33:15 +0200

    Добавьте файлы проекта.

---
+++ hththt/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyLibsss;
+
+namespace hththt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LucidDreams lucidDreams = new LucidDreams(); lucidDreams.Correct();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LucidDreams lucidDreams = new LucidDreams(); lucidDreams.InCorrect();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Witchblades witchblades = new Witchblades(); witchblades.Correct();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Witchblades witchblades = new Witchblades(); witchblades.InCorrect();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            BankAccount bankAccount = new BankAccount(); bankAccount.Correct();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            BankAccount bankAccount = new BankAccount(); bankAccount.InCorrect();
+        }
+    }
+}
---
+++ hththt/Music.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows.Forms;
+namespace MyLibsss
+{
+    public abstract class Music
+    {
+        public string Name { get; set; }
+
+        public abstract void Correct();
+        public abstract void InCorrect();
+    }
+
+    
+    public class LucidDreams : Music
+    {
+        public override void Correct()
+        {
+            
+            MessageBox.Show("Правильно");
+        }
+
+        public override void InCorrect()
+        {
+            MessageBox.Show("Не правильно");
+        }
+    }
+
+    
+    public class Witchblades : Music
+    {
+        public override void Correct()
+        {
+            MessageBox.Show("Правильно");
+        }
+
+        public override void InCorrect()
+        {
+            MessageBox.Show("Не правильно");
+        }
+    }
+
+   
+    public class BankAccount : Music
+    {
+        public override void Correct()
+        {
+            MessageBox.Show("Правильно ");
+        }
+
+        public override void InCorrect()
+        {
+            MessageBox.Show("Не правильно ");
+        }
+    }
+
+   
+
+    
+}
